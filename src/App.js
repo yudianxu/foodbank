47c24e2f3acfd66ebad6203c75603e65dfd87d0e
Yudian Xu <yudianxu@Yudians-MacBook-Pro.local>
2022-02-25 11:43:20 -0800

    mid point

--- src/App.js
+++ src/App.js
@@ -1,54 +1,94 @@
-import "./App.css";
-// import TextFile from Material UI
-import TextField from "@mui/material/TextField";
-import { useState } from "react";
-import Button from "@mui/material/Button";
-import { BiLocationPlus } from 'react-icons/bi';
+import React from 'react';
+import { Input, Button, Select, Radio, message } from 'antd';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 
-function App() {
-  const [text, setText] = useState("");
-  const [gifs, setGifs] = useState([]);
 
-  // "async" means that you can now use the word
-  // "await" within this function!
-  async function search() {
-    const key = "LOXyL74y5GuIdwV0o8p6j4sILlXJ40iG";
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${text}&limit=25&offset=0&lang=en`;
-    const r = await fetch(url);
-    const j = await r.json();
-    setGifs(j.data);
+class NoteList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      zipCode: "",
+      type: "1",
+      lng: "36.8233",
+      lat: "-1.2884",
+      lngt: "36.8233",
+      latt: "-1.2884",
+    }
+
   }
 
-  console.log(gifs);
-  return (
-    <div className="App">
-      <div className="searchbar">
-        {/*  "outlined" variant prop gives the nice animation */}
-        <BiLocationPlus size="24" />
-        <TextField
-          id="search"
-          label="Enter you zip code"
-          variant="outlined"
-          onChange={(e) => setText(e.target.value)}
-          value={text}
-          onKeyPress={(e) => {
-            if (e.key === "Enter") search();
-            <BiLocationPlus size="24" />
-          }}
-        />
-        <Button variant="outlined" onClick={search} size="large">
-          Search
-        </Button>
+  // detect whether the user has logged in
+  componentWillMount() {
+
+  }
+
+
+
+  render() {
+    const {type,lng,lat,zipCode,mapkey,latt,lngt} = this.state;
+    const mapStyles = {
+      width: '100%',
+      height: '100%'
+    }
+    return (
+      <div className='home-container' style={{ height: "100%", boxSizing: 'border-box' }}>
+        <div style={{ fontSize: 20 }}>
+          <div>
+            <Radio.Group style={{ margin: 10 }} onChange={(e) => {
+              console.log("e===",e);
+              this.setState({
+                type: e.target.value
+              })
+            }} value={type} defaultValue={type}>
+              <Radio.Button value="1">Position</Radio.Button>
+              <Radio.Button value="2">ZipCode</Radio.Button>
+            </Radio.Group>
+            {type === "1" ? <>
+              <input value={latt} placeholder="lat" onChange={(e)=>{
+                this.setState({
+                  latt:e.target.value
+                })
+              }}></input>
+              <input value={lngt} placeholder="lng" onChange={(e)=>{
+                this.setState({
+                  lngt:e.target.value
+                })
+              }}></input></> : <input value={zipCode} placeholder="zipcode"></input>}
+
+              <Button type="primary" onClick={(e)=>{
+                this.setState({
+                  mapkey:mapkey+1,
+                  lat:latt,
+                  lng:lngt,
+                })
+              }}>confirm</Button>
+          </div>
+
+          <div style={{ position: "relative", width: 800, height: 500 }}>
+            <Map
+              google={this.props.google}
+              zoom={14}
+              key={mapkey}
+              style={mapStyles}
+              initialCenter={{
+                lat:lat,
+                lng: lng
+              }}
+            >
+              <Marker
+                title={'The marker`s title will appear as a tooltip.'}
+                name={'SOMA'}
+                position={{lat: lat, lng: lng}} />
+            </Map>
+          </div>
+        </div>
       </div>
-      <div className="gifs">
-        {gifs.map((gif, i) => {
-          return <img key={i} src={gif.images.fixed_height.url} />;
-        })}
-      </div>
-    </div>
-  );
+    )
+
+  }
 }
-
-export default App;
-
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBxzEsUs6BFw9GsLkkSMj1OpirjlfyvEtc'
+})(NoteList);
